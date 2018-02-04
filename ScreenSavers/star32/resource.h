a50bde4ac148b312d4a09a9f246b65b956c3df86
bob frazier <bobf@mrp3.com>
2018-02-03 19:19:28 -0800

    added

---
+++ ScreenSavers/star32/resource.h
@@ -0,0 +1,24 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by STAR32.RC
+//
+#define IDD_ABOUT_SFT                   101
+#define ID_STARS                        201
+#define ID_WARP                         202
+#define ID_INTERVAL                     203
+#define IDC_ABOUT                       1000
+#define IDC_MUSIC                       1002
+#define IDC_ABOUT_TEXT                  1003
+#define IDC_ANAGRAPH                    1004
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
